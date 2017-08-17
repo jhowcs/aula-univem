1a702f1a20ec3306a17a5dec6ec17205d10d41b9
Jonathan Campos <jhowcs@gmail.com>
2017-08-16 21:19:16 -0300

    Adiciona TODOs

--- Aula01-Serializando/app/src/main/java/com/jhowcs/aula01_serializando/MainActivity.java
+++ Aula01-Serializando/app/src/main/java/com/jhowcs/aula01_serializando/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String EXTRA_STOCK = "extra_stock";
 
+    // TODO: 8/16/17 crie 2 campos do tipo EditText
+
     private Button btnActivity2;
 
     @Override
@@ -18,15 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnActivity2 = (Button) findViewById(R.id.btnActivity2);
+        //// TODO: 8/16/17 inicialize os campos de texto criados
 
         btnActivity2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: 8/16/17 crie uma instância de StockSerializable com os valores informados nos edittexts
+
+                // TODO: 8/16/17 chame o método newIntent estático criado em MainActivity2 dentro de startActivity()
                 abrirActivity2();
             }
         });
     }
 
+    // TODO: 8/16/17 remova este método
     private void abrirActivity2() {
         Intent it = new Intent(this, MainActivity2.class);
         it.putExtra(EXTRA_STOCK, new StockSerializable("USIM5", 5.89));
--- Aula01-Serializando/app/src/main/java/com/jhowcs/aula01_serializando/MainActivity2.java
+++ Aula01-Serializando/app/src/main/java/com/jhowcs/aula01_serializando/MainActivity2.java
@@ -9,14 +9,24 @@ public class MainActivity2 extends AppCompatActivity {
     private static final String EXTRA_STOCK = "extra_stock";
     private static final String TAG = "MainActivity2";
 
+    // TODO: 8/16/17 crie 2 campos do tipo TextView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        // TODO: 8/16/17 inicialize os campos de texto criados
+
         StockSerializable stock
                 = (StockSerializable) getIntent().getSerializableExtra(EXTRA_STOCK);
 
+
+
         Log.d(TAG, stock.toString());
     }
+
+    // TODO: 8/16/17 crie um método que receba um stockSerializable e preencha o textViews
+
+    // TODO: 8/16/17 adicione um método público estático newIntent(StockSerializable stock) que retorne uma intent
 }
