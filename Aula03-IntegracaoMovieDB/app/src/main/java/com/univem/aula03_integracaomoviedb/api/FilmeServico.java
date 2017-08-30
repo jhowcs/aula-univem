6566791bb148ce59ce8c8ecba3c024510b1a6e95
Jonathan Campos <jhowcs@gmail.com>
2017-08-29 21:40:07 -0300

    Adiciona serviço
    - Foi adicionada a interface FilmeServico
      para permitir buscar os filmes populares da API

---
+++ Aula03-IntegracaoMovieDB/app/src/main/java/com/univem/aula03_integracaomoviedb/api/FilmeServico.java
@@ -0,0 +1,14 @@
+package com.univem.aula03_integracaomoviedb.api;
+
+
+import com.univem.aula03_integracaomoviedb.model.FilmeResultados;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FilmeServico {
+
+    @GET("movie/popular")
+    Call<FilmeResultados> getFilmesPopulares(@Query("api_key") String apiKey);
+}
