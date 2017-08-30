fe0a0f2da39872dc5fe391cc99162fd4cf7712d7
Jonathan Campos <jhowcs@gmail.com>
2017-08-29 23:58:50 -0300

    Adiciona mock de resposta filmes populares

---
+++ Aula03-IntegracaoMovieDB/app/src/androidTest/java/com/univem/aula03_integracaomoviedb/FilmesPopularesMock.java
@@ -0,0 +1,52 @@
+package com.univem.aula03_integracaomoviedb;
+
+public interface FilmesPopularesMock {
+    String mock = "{\n" +
+            "   \"page\":1,\n" +
+            "   \"total_results\":19472,\n" +
+            "   \"total_pages\":974,\n" +
+            "   \"results\":[\n" +
+            "      {\n" +
+            "         \"vote_count\":4261,\n" +
+            "         \"id\":211672,\n" +
+            "         \"video\":false,\n" +
+            "         \"vote_average\":6.4,\n" +
+            "         \"title\":\"Minions\",\n" +
+            "         \"popularity\":300.288453,\n" +
+            "         \"poster_path\":\"\\/q0R4crx2SehcEEQEkYObktdeFy.jpg\",\n" +
+            "         \"original_language\":\"en\",\n" +
+            "         \"original_title\":\"Minions\",\n" +
+            "         \"genre_ids\":[\n" +
+            "            10751,\n" +
+            "            16,\n" +
+            "            12,\n" +
+            "            35\n" +
+            "         ],\n" +
+            "         \"backdrop_path\":\"\\/uX7LXnsC7bZJZjn048UCOwkPXWJ.jpg\",\n" +
+            "         \"adult\":false,\n" +
+            "         \"overview\":\"Minions Stuart, Kevin and Bob are recruited by Scarlet Overkill, a super-villain who, alongside her inventor husband Herb, hatches a plot to take over the world.\",\n" +
+            "         \"release_date\":\"2015-06-17\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "         \"vote_count\":4922,\n" +
+            "         \"id\":321612,\n" +
+            "         \"video\":false,\n" +
+            "         \"vote_average\":6.8,\n" +
+            "         \"title\":\"Beauty and the Beast\",\n" +
+            "         \"popularity\":150.620218,\n" +
+            "         \"poster_path\":\"\\/tWqifoYuwLETmmasnGHO7xBjEtt.jpg\",\n" +
+            "         \"original_language\":\"en\",\n" +
+            "         \"original_title\":\"Beauty and the Beast\",\n" +
+            "         \"genre_ids\":[\n" +
+            "            10751,\n" +
+            "            14,\n" +
+            "            10749\n" +
+            "         ],\n" +
+            "         \"backdrop_path\":\"\\/6aUWe0GSl69wMTSWWexsorMIvwU.jpg\",\n" +
+            "         \"adult\":false,\n" +
+            "         \"overview\":\"A live-action adaptation of Disney's version of the classic 'Beauty and the Beast' tale of a cursed prince and a beautiful young woman who helps him break the spell.\",\n" +
+            "         \"release_date\":\"2017-03-16\"\n" +
+            "      }\n" +
+            "   ]\n" +
+            "}";
+}
