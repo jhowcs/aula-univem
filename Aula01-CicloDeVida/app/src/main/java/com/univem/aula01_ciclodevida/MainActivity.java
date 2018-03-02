b3bc428097f6d9af0945038fd28a26a5fa27a081
Jonathan Campos <jhowcs@gmail.com>
2018-03-01 22:23:42 -0300

    Adiciona projeto Ciclo de Vida

---
+++ Aula01-CicloDeVida/app/src/main/java/com/univem/aula01_ciclodevida/MainActivity.java
@@ -0,0 +1,15 @@
+package com.univem.aula01_ciclodevida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // TODO: logar todos os ciclos de vida
+}
